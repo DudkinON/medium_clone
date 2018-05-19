05591251c3feaece820792036a83e2fa14865c30
Oleg Dudkin <dudkin.on@gmail.com>
2018-05-19 16:11:36 -0500

    feat: Add User model

--- app/models.py
+++ app/models.py
@@ -4,6 +4,7 @@
 from __future__ import unicode_literals
 
 from sqlalchemy import Column, String, Boolean, Integer, ForeignKey, DateTime
+from sqlalchemy import Text
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -29,6 +30,21 @@ engine = create_engine(connect)
 Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
+
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column('id', Integer, primary_key=True)
+    first_name = Column('first_name', String(60))
+    last_name = Column('last_name', String(60))
+    email = Column('email', String(45))
+    hash = Column('hash', String(250))
+    is_active = Column('is_active', Boolean, default=False)
+    last_visit = Column('last_visit', DateTime, onupdate=datetime.utcnow)
+    register_date = Column('register_date', DateTime)
+    is_staff = Column('is_staff', Boolean, default=False)
+    is_super_user = Column('is_super_user', Boolean, default=False)
+
 
 # create an engine
 engine = create_engine(connect)
