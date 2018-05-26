15cf0ca1a4b47b758d643ba007911cd901c6312a
Oleg Dudkin <dudkin.on@gmail.com>
2018-05-26 14:42:12 -0500

    feat: Add database config to settings

--- app/settings.py
+++ app/settings.py
@@ -2,3 +2,14 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import unicode_literals
+from os import path
+from .keys import user_name, user_password, database_name, host
+
+BASE_DIR = path.dirname(path.realpath(__file__))
+
+db = {
+    'user': user_name,
+    'password': user_password,
+    'database': database_name,
+    'host': host
+}
