27da8b136288fbab4f75ee4f8e9d4b59bd7774fd
Oleg Dudkin <dudkin.on@gmail.com>
2018-05-29 05:59:01 -0500

    docs: Add a docstring to get_user_by_email function

--- app/provider.py
+++ app/provider.py
@@ -4,5 +4,9 @@ query = session.query
 
 
 def get_user_by_email(email):
-
+    """
+    Return user by email or None
+    :param email: String
+    :return object:
+    """
     return query(User).filter_by(email=email).first() or None
