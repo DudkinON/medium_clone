ce1770bf766be15e184422d2ec922a5a8617383d
Oleg Dudkin <dudkin.on@gmail.com>
2018-05-30 03:49:25 -0500

    feat: Add get_unique_str function

--- app/resource.py
+++ app/resource.py
@@ -2,3 +2,17 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import unicode_literals
+
+from random import choice
+from string import ascii_uppercase as uppercase, digits
+
+
+def get_unique_str(amount):
+
+    try:
+        unique_string = (''.join(choice(uppercase + digits)
+                                 for x in xrange(amount)))
+    except NameError:
+        unique_string = (''.join(choice(uppercase + digits)
+                                 for x in range(amount)))
+    return unique_string
